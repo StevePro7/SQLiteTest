6c26a9cbc3984e48b84346b7fef5a89d4f42b293
StevePro7 <steveprozune@hotmail.com>
2018-09-12 13:41:16 +0100

    Add code changes

--- MySQLiteTest/MySQLiteTest/DataContext.cs
+++ MySQLiteTest/MySQLiteTest/DataContext.cs
@@ -1,9 +1,37 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Data.Sqlite;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace MySQLiteTest
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        private string _databasePath = "";
+
+        public DataContext() : base()
+        {
+        }
+
+        public DataContext(string databasePath)
+        {
+            _databasePath = databasePath;
+            Database.EnsureCreated();
+
+            //// Android
+            //var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "invoiceje.db");
+
+            //// UWP
+            //var dbPath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "exrin.db");
+
+            //// iOS
+            //var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", "exrin.db");
+        }
+
+        public DbSet<Invoice> Invoices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = _databasePath;
+            //optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
     }
+
 }
--- MySQLiteTest/MySQLiteTest/ViewController.cs
+++ MySQLiteTest/MySQLiteTest/ViewController.cs
@@ -1,12 +1,14 @@
-﻿using Foundation;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UIKit;
 
 namespace MySQLiteTest
 {
     public partial class ViewController : UIViewController
     {
+        private DataContext dataContext;
         private DataSource dataSource;
 
         public ViewController(IntPtr handle) : base(handle)
@@ -26,10 +28,43 @@ namespace MySQLiteTest
             MyUpdateButton.TouchUpInside += MyUpdateButton_TouchUpInside;
             MyRemoveButton.TouchUpInside += MyRemoveButton_TouchUpInside;
             MyExitButton.TouchUpInside += MyExitButton_TouchUpInside;
+
+            var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", "exrin.db");
+            dataContext = new DataContext(dbPath);
         }
 
         public override void ViewWillAppear(bool animated)
         {
+            IList<Invoice> invoices = dataContext.Invoices.ToList();
+            Invoice invoice1 = dataContext.Invoices.Find(123);
+            Invoice invoice4 = dataContext.Invoices.Find(456);
+
+            if (invoice1 == null)
+            {
+                Invoice inv = new Invoice { Id = 123, Name = "Steven", };
+                dataContext.Invoices.Add(inv);
+            }
+            if (invoice4 == null)
+            {
+                Invoice inv2 = new Invoice { Id = 456, Name = "StevePro", };
+                dataContext.Invoices.Add(inv2);
+            }
+
+            if (invoice1 != null)
+            {
+                invoice1.Name = "Suzanne";
+                dataContext.Invoices.Update(invoice1);
+            }
+            if (invoice4 != null)
+            {
+                invoice4.Name = "Adriana";
+                dataContext.Invoices.Update(invoice4);
+
+                //dataContext.Invoices.Remove(invoice4);
+            }
+
+            dataContext.SaveChanges();
+
             base.ViewWillAppear(animated);
         }
 
